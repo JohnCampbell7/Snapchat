372ebcab6e80a9c8fdac96819b97b40ffc0b40b5
John Campbell <johnc7910@gmail.com>
2017-09-19 22:24:33 +0100

    added Firebase and Sign In design

---
+++ Snapchat/SignInViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SignInViewController.swift
+//  Snapchat
+//
+//  Created by John Campbell on 19/09/2017.
+//  Copyright © 2017 John Campbell. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+    @IBAction func signInTapped(_ sender: Any) {
+    }
+
+
+}
+
