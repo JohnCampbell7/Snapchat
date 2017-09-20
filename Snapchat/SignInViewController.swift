d00412ea5630610d11eb983b6af05d354a10aa4f
John Campbell <johnc7910@gmail.com>
2017-09-20 22:18:10 +0100

    sign in code and create user added

--- Snapchat/SignInViewController.swift
+++ Snapchat/SignInViewController.swift
@@ -7,20 +7,42 @@
 //
 
 import UIKit
+import Firebase
 
 class SignInViewController: UIViewController {
-
+    
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     
     @IBAction func signInTapped(_ sender: Any) {
+        
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!, completion: { (user, error) in
+            print("We tried to sign in")
+            if error != nil {
+                print("Hey we have an error:\(error)")
+                
+                Auth.auth().createUser(withEmail: self.emailTextField.text!, password:  self.passwordTextField.text!, completion: { (user, error) in
+                    print("We tried to create a user")
+                    if error != nil {
+                        print("Hey we have an error:\(error)")
+                    } else {
+                        print("Created user successfully")
+                    }
+                })
+                
+            } else {
+                print("Signed in successfully")
+                self.performSegue(withIdentifier: "signinsegue", sender: nil)
+            }
+        })
+        
     }
-
-
+    
+    
 }
 
