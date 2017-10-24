43aece1b419ba4e6b0e65a9c5fc1845066c1a7f6
John Campbell <johnc7910@gmail.com>
2017-10-24 22:15:00 +0100

    Image folder integration with Firebase Storage created but with error happening when trying to click next on upload snap image

--- Snapchat/SignInViewController.swift
+++ Snapchat/SignInViewController.swift
@@ -24,12 +24,12 @@ class SignInViewController: UIViewController {
         Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!, completion: { (user, error) in
             print("We tried to sign in")
             if error != nil {
-                print("Hey we have an error:\(error))")
+                print("Hey we have an error:\(error)")
                 
                 Auth.auth().createUser(withEmail: self.emailTextField.text!, password:  self.passwordTextField.text!, completion: { (user, error) in
                     print("We tried to create a user")
                     if error != nil {
-                        print("Hey we have an error:\(error))")
+                        print("Hey we have an error:\(error)")
                     } else {
                         print("Created user successfully!")
                     }
